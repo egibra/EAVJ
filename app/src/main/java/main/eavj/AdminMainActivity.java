b6e55beb9e484950935aef5261268db55fd84729
Egidijus <egidijus.brazaitis@gmail.com>
2018-03-17 09:15:49 +0200

    Added admin panel and its background

---
+++ app/src/main/java/main/eavj/AdminMainActivity.java
@@ -0,0 +1,13 @@
+package main.eavj;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AdminMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_main);
+    }
+}

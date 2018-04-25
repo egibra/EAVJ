cd157e135ce8628358e27cb94a0590f3a7fe6727
VIKA\vika <vikraz@ktu.lt>
2018-04-25 17:53:28 +0300

    view trip list (edition)

---
+++ app/src/main/java/main/eavj/TripDetailActivity.java
@@ -0,0 +1,19 @@
+package main.eavj;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by vika on 25-Apr-18.
+ */
+
+public class TripDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)  {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_detail);
+
+
+    }
+}
--- app/src/main/java/main/eavj/TripIdeasActivity.java
+++ app/src/main/java/main/eavj/TripIdeasActivity.java
@@ -1,7 +1,9 @@
 package main.eavj;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by vika on 14-Mar-18.
@@ -15,5 +17,11 @@ public class TripIdeasActivity extends Activity{
         setContentView(R.layout.activity_trip_ideas);
 
 
+    }
+
+    public void openTripDetail(View view)
+    {
+        Intent intent = new Intent(this, TripDetailActivity.class);
+        startActivity(intent);
     }
 }
